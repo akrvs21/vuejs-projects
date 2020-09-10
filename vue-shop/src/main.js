c2213313e8870f6dc7906964b897013dff999ebd
raider2107 <3108987378@qq.com>
2020-09-10 17:39:02 +0800

    desing

---
+++ vue-shop/src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import 'bootstrap';
+import './assets/app.scss';
+import jQuery from 'jquery'
+import 'popper.js'
+
+window.$ = window.jQuery = jQuery;
+
+Vue.component('Navbar',require('./components/Navbar.vue').default);
+Vue.component('Hero',require('./components/Hero.vue').default);
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
