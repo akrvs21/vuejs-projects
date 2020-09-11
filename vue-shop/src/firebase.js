60a16d85c402d003db654697d4ccfd0c9b9f9ff3
raider2107 <3108987378@qq.com>
2020-09-11 17:11:49 +0800

    login

---
+++ vue-shop/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA-6UTBSsm1msvjhGZZ27ci24zwTfFfJWs",
+    authDomain: "vue-shop-7ed4f.firebaseapp.com",
+    databaseURL: "https://vue-shop-7ed4f.firebaseio.com",
+    projectId: "vue-shop-7ed4f",
+    storageBucket: "vue-shop-7ed4f.appspot.com",
+    messagingSenderId: "305741859130",
+    appId: "1:305741859130:web:2e180a831c4837f3a4a803"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
