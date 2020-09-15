8b4a0222e482997441dd068ec4ed619884a85cd9
raider2107 <3108987378@qq.com>
2020-09-16 00:07:04 +0800

    product page

--- vue-shop/src/views/Admin.vue
+++ vue-shop/src/views/Admin.vue
@@ -2,7 +2,8 @@
     <!-- eslint-disable  -->
     <div class="admin">
         <div class="page-wrapper default-theme sidebar-bg bg1 toggled">
-            <nav id="sidebar" class="sidebar-wrapper">
+            <nav id="sidebar"
+                 class="sidebar-wrapper">
                 <div class="sidebar-content">
                     <!-- sidebar-brand  -->
                     <div class="sidebar-item sidebar-brand">
@@ -11,16 +12,18 @@
                     <!-- sidebar-header  -->
                     <div class="sidebar-item sidebar-header d-flex flex-nowrap">
                         <div class="user-pic">
-                            <img class="img-responsive img-rounded" src="img/user.jpg" alt="User picture">
+                            <img class="img-responsive img-rounded"
+                                 src="img/user.jpg"
+                                 alt="User picture">
                         </div>
                         <div class="user-info">
                             <span class="user-name">Jhon
-                                                                                        <strong>Smith</strong>
-                                                                                    </span>
+                                <strong>Smith</strong>
+                            </span>
                             <span class="user-role">Administrator</span>
                             <span class="user-status">
-                                                                                        <i class="fa fa-circle"></i>
-                                                                                        <span>Online</span>
+                                <i class="fa fa-circle"></i>
+                                <span>Online</span>
                             </span>
                         </div>
                     </div>
@@ -28,11 +31,14 @@
                     <div class="sidebar-item sidebar-search">
                         <div>
                             <div class="input-group">
-                                <input type="text" class="form-control search-menu" placeholder="Search...">
+                                <input type="text"
+                                       class="form-control search-menu"
+                                       placeholder="Search...">
                                 <div class="input-group-append">
                                     <span class="input-group-text">
-                                                        <i class="fa fa-search" aria-hidden="true"></i>
-                                                    </span>
+                                        <i class="fa fa-search"
+                                           aria-hidden="true"></i>
+                                    </span>
                                 </div>
                             </div>
                         </div>
@@ -77,7 +83,8 @@
             <!-- page content -->
             <div class="page-content">
                 <h3 class="d-inline-block">Product List</h3>
-                <button @click="addNew" class="btn btn-primary float-right">Add product</button>
+                <button @click="addNew"
+                        class="btn btn-primary float-right">Add product</button>
                 <table class="table">
                     <thead>
                         <tr>
@@ -95,51 +102,98 @@
                                 {{ product.price }}
                             </td>
                             <td>
-                                <button @click="editProduct(product)" class="btn btn-primary">Edit</button>
-                                <button @click="deleteProduct(product)" class="btn btn-danger">Delete</button>
+                                <button @click="editProduct(product)"
+                                        class="btn btn-primary">Edit</button>
+                                <button @click="deleteProduct(product)"
+                                        class="btn btn-danger">Delete</button>
                             </td>
                         </tr>
                     </tbody>
                 </table>
             </div>
             <!-- Modal -->
-            <div class="modal fade" id="myModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal fade"
+                 id="myModal"
+                 data-backdrop="static"
+                 data-keyboard="false"
+                 tabindex="-1"
+                 aria-labelledby="staticBackdropLabel"
+                 aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="staticBackdropLabel">Edit Product</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                      <span aria-hidden="true">&times;</span>
-                                                    </button>
+                            <h5 class="modal-title"
+                                id="staticBackdropLabel">Edit Product</h5>
+                            <button type="button"
+                                    class="close"
+                                    data-dismiss="modal"
+                                    aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
                         </div>
                         <div class="modal-body">
                             <div class="input-group mb-3">
-                                <input type="text" class="form-control" v-model="product.name" placeholder="Product Name" aria-label="Product Name" aria-describedby="basic-addon1">
+                                <input type="text"
+                                       class="form-control"
+                                       v-model="product.name"
+                                       placeholder="Product Name"
+                                       aria-label="Product Name"
+                                       aria-describedby="basic-addon1">
                             </div>
                             <div class="input-group">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text">Product description</span>
                                 </div>
-                                <textarea class="form-control" v-model="product.description" aria-label="With textarea"></textarea>
+                                <vue-editor v-model="product.description"></vue-editor>
                             </div>
-                            <h5 class="modal-title" style="margin-bottom:12px; margin-top:12px" id="staticBackdropLabel">Product Detail</h5>
+                            <h5 class="modal-title"
+                                style="margin-bottom:12px; margin-top:12px"
+                                id="staticBackdropLabel">Product Detail</h5>
                             <div class="input-group mb-3">
-                                <input type="text" class="form-control" v-model="product.price" placeholder="Product price" aria-label="Product price" aria-describedby="basic-addon2">
+                                <input type="text"
+                                       class="form-control"
+                                       v-model="product.price"
+                                       placeholder="Product price"
+                                       aria-label="Product price"
+                                       aria-describedby="basic-addon2">
                             </div>
                             <div class="input-group mb-3">
-                                <input type="text" placeholder="Product tags" v-model="product.tags" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+                                <input @keyup.32="addTag"
+                                       type="text"
+                                       placeholder="Product tags"
+                                       v-model="tag"
+                                       class="form-control"
+                                       id="basic-url"
+                                       aria-describedby="basic-addon3">
+                            </div>
+                            <div class="tag d-inline-block" v-for="tag in product.tags">
+                                <span>
+                                    {{ tag }}
+                                </span>
                             </div>
                             <div class="input-group">
                                 <div class="custom-file">
-                                    <input type="file" class="custom-file-input" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04">
-                                    <label class="custom-file-label" for="inputGroupFile04">Choose file</label>
+                                    <input type="file"
+                                           class="custom-file-input"
+                                           id="inputGroupFile04"
+                                           aria-describedby="inputGroupFileAddon04">
+                                    <label class="custom-file-label"
+                                           for="inputGroupFile04">Choose file</label>
                                 </div>
                             </div>
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button @click="saveNewProduct" v-if="editMode == 'new'" type="button" class="btn btn-primary">Save product</button>
-                            <button @click="updateProductInfo" v-if="editMode == 'edit'" type="button" class="btn btn-primary">Update changes</button>
+                            <button type="button"
+                                    class="btn btn-secondary"
+                                    data-dismiss="modal">Close</button>
+                            <button @click="saveNewProduct"
+                                    v-if="editMode == 'new'"
+                                    type="button"
+                                    class="btn btn-primary">Save product</button>
+                            <button @click="updateProductInfo"
+                                    v-if="editMode == 'edit'"
+                                    type="button"
+                                    class="btn btn-primary">Update changes</button>
                         </div>
                     </div>
                 </div>
@@ -157,10 +211,14 @@
         fb,
         db
     } from '../firebase.js'
+    import { VueEditor } from "vue2-editor";
     export default {
         name: 'Admin',
         props: {
             msg: String
+        },
+        components: {
+            VueEditor
         },
         data() {
             return {
@@ -169,9 +227,10 @@
                     name: null,
                     price: null,
                     description: null,
-                    tags: null,
+                    tags: [],
                     images: null
                 },
+                tag: null,
                 editMode: null,
             }
         },
@@ -181,6 +240,10 @@
             }
         },
         methods: {
+            addTag() {
+                this.product.tags.push(this.tag)
+                this.tag = ""
+            },
             reset() {
                 this.product = {
                     name: null,
@@ -199,7 +262,7 @@
             editProduct(product) {
                 this.editMode = 'edit';
                 console.log(this.editMode)
-                console.log(product)           
+                console.log(product)
                 this.product = product
                 $('#myModal').modal('show')
             },
@@ -208,9 +271,9 @@
                 this.$firestore.products.doc(this.product.id).update(this.product)
                 $('#myModal').modal('hide')
                 Toast.fire({
-                            icon: 'success',
-                            title: 'Updated successfully'
-                        })
+                    icon: 'success',
+                    title: 'Updated successfully'
+                })
             },
             deleteProduct(item) {
                 this.product = item
@@ -239,7 +302,7 @@
                 Toast.fire({
                     icon: 'success',
                     title: 'Added successfully'
-            })
+                })
             }
         }
     }
@@ -247,5 +310,20 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+.tag {
+    background-color:rgb(203, 209, 216);
+    padding:10px;
+    line-height: 9px;
+    border-color:white;
+    border-width: 3px;
+    border-style: solid;
+    font-size:2em;
+    margin-bottom: 10px;
+    color: white;
+}
+.tag span{
+    color: black;
+    font-style: italic;
+    font-size: 20px;
+}
 </style>
